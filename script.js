e79e1e1132b790de0c692786d04aa6b23ba6ac20
Fall_akashi <flakrasgappk@gmail.com>
2024-01-13 14:54:59 -0300

    script

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let helloWord = "hello,word";
+console.log(helloWord);
+
+let userName = "Fall"
+console.log(userName);
+
+let age = 20;
+
+console.log("meu nome é " +  userName + " e tenho " + age + " anos " )
+
+console.log(`meu nome é ${userName} e tenho ${age} anos`);
