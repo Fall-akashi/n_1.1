1f4ecbf63d53ec9732ccbbc6e0a8b3e39a9dcc7a
Fall_akashi <flakrasgappk@gmail.com>
2024-01-27 14:53:39 -0300

    att2

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 
-let helloWord = "hello,word";
-console.log(helloWord);
+//let helloWord = "hello,word";
+//console.log(helloWord);
 
-let userName = prompt("digite o seu nome")
-console.log(userName);
+//let userName = prompt("digite o seu nome")
+//console.log(userName);
 
-let menssage ="bem vindo"
-console.log(`ola ${userName} seja bem vindo`)
+//let menssage = "bem vindo"
+//console.log(`ola ${userName} seja bem vindo`)
 
-let age = 20;
-let student = false;
+//let age = 20;
+//let student = false;
 
-console.log("meu nome é " +  userName + " e tenho " + age + " anos " )
+//console.log("meu nome é " + userName + " e tenho " + age + " anos ")
 
-console.log(`meu nome é ${userName} e tenho ${age} anos`);
+//console.log(`meu nome é ${userName} e tenho ${age} anos`);
+
+//let firstNumber = +prompt("digite o primeiro numero")
+
+//let secondNumber = +prompt("digite o segundo numero")
+
+//console.log(firstNumber + secondNumber)
+
+//let base = +prompt("digite o numero da base")
+//let altura = +prompt("digite a aulura")
+
+//console.log(`à area do rentangulo é: ${base * altura} cm²`);
+let userName ="fall"
+let number = 20
+let isStudent = false
+
+let user=[userName, number,isStudent,"valor direto",18, true];
+
+user[3]= "test"
+
+function acessIndex(){
+
+    let index = +prompt("digite o index")
+
+    alert(`o elemento do idex desejado é ${user[index]}`)
+
+}
+
